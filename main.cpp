5688fb34c9457fe1c801328ceeeda34802e25f25
BlackPrince18 <mihneateodor18@gmail.com>
2024-02-26 14:08:44 +0200

    Meniul Principal

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//  Meniul Principal
+
+    cout<<"\t\tManagement Atelier Auto\t\t"<<endl<<endl;
+    cout<<"I.\tAngajati"<<endl;
+    cout<<"II.\tMasini"<<endl;
+    cout<<"III.\tActivitatea Atelierului"<<endl;
+    cout<<"IV.\tRaportare"<<endl;
+
+    getchar();
+    return 0;
+}
